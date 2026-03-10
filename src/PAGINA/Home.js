ca249c5a35bfc2fad62e88d3b02f6015ecf2a159
JeanpollaTI <nanipollo38@gmail.com>
2026-03-09 20:43:54 -0700

    Final Scholaris UI refinements

--- src/PAGINA/Home.js
+++ src/PAGINA/Home.js
@@ -311,9 +311,9 @@ function Home({ user }) {
 
         <div className="home-container container grid">
           <div className="home-data">
-            <h1 className="home-title" style={{ color: '#ffffff' }}>
+            <h1 className="home-title">
               {user ? (
-                <>Bienvenido <span style={{ color: '#ffffff' }}>{primerNombre}</span> al sistema de <span>gestión académica</span></>
+                <>Bienvenido <span className="user-name-gold">{primerNombre}</span> al sistema de <span>gestión académica</span></>
               ) : (
                 <>Bienvenido al sistema de <span>gestión académica</span></>
               )}
